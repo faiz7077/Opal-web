0392ed1411356b5f99240a4ed2b348d3e0d098f2
faiz7077 <faizanshaikh008007@gmail.com>
2025-01-26 15:32:30 +0530

    Change in layout

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Layout = ({children}: Props) => {
   return (
-    <div className="flex flex-col py-10 px-10 xl:px-0 container">
+    <div className="flex flex-col  xl:px-0 container">
         {children}
     </div>
   )
